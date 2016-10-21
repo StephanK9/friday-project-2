5f4b09f864465c07952e76f8c4f6d450ee4ea45a
Stephan Khoury <stephane.khoury@gmail.com>
2016-10-21 11:16:31 -0700

    add images that show up after the alert messages

--- js/scripts.js
+++ js/scripts.js
@@ -5,52 +5,53 @@
       var q2  = $("input:radio[name=q2]:checked").val();
       var q3  = $("input:radio[name=q3]:checked").val();
       var q4  = $("input:radio[name=q4]:checked").val();
+      var q5  = $("input:radio[name=q5]:checked").val();
 
     if (q1 === "css1" && q2 === "css2" && q3 === "css3" && q4 === "css4") {
     alert("You should take the CSS/Design Track!");
-    $("#jack").show();
+    $("#css").show();
   } else if (q1 === "css1" && q2 === "css2" && q3 === "css3") {
     alert("You should take the CSS/Design Track!");
-    $("#jack").show();
+    $("#css").show();
   } else if (q1 === "css1" && q4 === "css4" && q3 === "css3") {
     alert("You should take the CSS/Design Track!");
-    $("#jack").show();
+    $("#css").show();
   } else if (q2 === "css2" && q4 === "css4" && q3 === "css3") {
     alert("You should take the CSS/Design Track!");
-    $("#jack").show();
+    $("#css").show();
   } else if (q2 === "css2" && q4 === "css4" && q1 === "css1") {
     alert("You should take the CSS/Design Track!");
-    $("#jack").show();
+    $("#css").show();
   } else if (q1 === "php1" && q2 === "php2" && q3 === "php3" && q4 === "php4") {
     alert("You should take the PHP/Drupal Track!");
-    $("#larry").show();
+    $("#php").show();
   } else if (q1 === "php1" && q2 === "php2" && q3 === "php3") {
     alert("You should take the PHP/Drupal Track!");
-    $("#larry").show();
+    $("#php").show();
   } else if (q1 === "php1" && q4 === "php4" && q3 === "php3") {
     alert("You should take the PHP/Drupal Track!");
-    $("#larry").show();
+    $("#php").show();
   } else if (q2 === "php2" && q4 === "php4" && q3 === "php3") {
     alert("You should take the PHP/Drupal Track!");
-    $("#larry").show();
+    $("#php").show();
   } else if (q2 === "php2" && q4 === "php4" && q1 === "php1") {
     alert("You should take the PHP/Drupal Track!");
-    $("#larry").show();
+    $("#php").show();
   } else if (q1 === "c1" && q2 === "c2" && q3 === "c3" && q4 === "c4") {
     alert("You should take the C#/.Net Track!");
-    $("#Miley").show();
+    $("#c").show();
   } else if (q1 === "c1" && q2 === "c2" && q3 === "c3") {
     alert("You should take the C#/.Net Track!");
-    $("#Miley").show();
+    $("#c").show();
   } else if (q1 === "c1" && q4 === "c4" && q3 === "c3") {
     alert("You should take the C#/.Net Track!");
-    $("#Miley").show();
+    $("#c").show();
   } else if (q2 === "c2" && q4 === "c4" && q3 === "c3") {
     alert("You should take the C#/.Net Track!");
-    $("#Miley").show();
+    $("#c").show();
   } else if (q2 === "c2" && q4 === "c4" && q1 === "c1") {
     alert("You should take the C#/.Net Track!");
-    $("#Miley").show();
+    $("#c").show();
   } else {
     alert("You don't have a clear preference.");
   };
